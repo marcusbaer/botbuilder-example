2e1319522780781d85e2e75bb7d9df7a961baca5
Marcus Baer <marcus.baer@publicispixelpark.de>
2018-06-28 17:21:56 +0200

    let bot start to talk

--- app.js
+++ app.js
@@ -13,13 +13,50 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 // let connector = new builder.ConsoleConnector().listen()
 let connector = new builder.ChatConnector({
   appId: process.env.MicrosoftAppId,
-  appPassword: process.env.MicrosoftAppPassword
+  appPassword: process.env.MicrosoftAppPassword,
+  openIdMetadata: process.env.BotOpenIdMetadata
 })
 
 server.post('/api/messages', connector.listen())
 
 let bot = new builder.UniversalBot(connector, (session) => {
-  session.send("You said: %s", session.message.text)
+  console.log(session.dialogStack(), session.message.text)
+  if (session.message.text) {
+    session.send("Root dialog. You said: %s", session.message.text)
+  } else {
+    session.send("Root dialog.")
+  }
+})
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/')
+            }
+        });
+    }
+})
+
+// bot.dialog('/', [
+//   (session) => {
+//     session.send('root dialog')
+//   }
+// ])
+
+// bot.dialog('root', [
+//   (session) => {
+//     session.send('root dialog')
+//   }
+// ])
+
+bot.dialog('level0-a', [
+  (session) => {
+    console.log(session.dialogStack(), session.message.text)
+    session.send('level0 A dialog')
+  }
+]).triggerAction({
+   matches: /^level0-a$/i
 })
 
 // https://docs.microsoft.com/en-us/azure/bot-service/nodejs/bot-builder-nodejs-quickstart?view=azure-bot-service-3.0
